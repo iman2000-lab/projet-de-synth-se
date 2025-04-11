3e4216cbf68071352636e779b095f9a64960e9d5
Lamiaa <fathallahlamiaa2004@gmail.com>
2025-04-11 15:57:48 +0200

    Ajout des modèles User, Product et Category par Lamiaa

---
+++ backend/app/Models/Product.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'quantity',
+        'image',
+        'category_id',
+        'is_featured',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
